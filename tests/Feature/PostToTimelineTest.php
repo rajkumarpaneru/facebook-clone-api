6ccf34a59c78eca91f19039ae86c5b59311f861b
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-04 20:05:13 +0545

    wip: endpoint to create post with tests

---
+++ tests/Feature/PostToTimelineTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostToTimelineTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_post_a_text_post()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+
+        $this->actingAs($user, 'api');
+
+        $response = $this->post('/api/posts', [
+            'data' => [
+                'type' => 'posts',
+                'attributes' => [
+                    'body' => 'New Post',
+                ]
+            ]
+        ]);
+
+        $post = \App\Models\Post::first();
+        $response->assertStatus(201);
+    }
+}
